275dd200a1622291134df9cc355b1654c56e8068
NicolasIssa1 <112622885+NicolasIssa1@users.noreply.github.com>
2024-03-03 19:39:57 +0000

    installing gradle pannel

---
+++ Coursework1/cwk1/src/main/java/Track.java
@@ -0,0 +1,55 @@
+/**
+ * Represents a point in space and time, recorded by a GPS sensor.
+ *
+ * @author Nicolas Issa
+ */
+public class Track {
+
+  // TODO: Create a stub for the constructor
+  public Track(){
+
+  }
+
+  public Track(String filename){
+
+  }
+
+  // TODO: Create a stub for readFile()
+  public void readFile(String filename) {
+  }
+
+  // TODO: Create a stub for add()
+  public void add(Point point){
+    return 0;
+  }
+
+  // TODO: Create a stub for get()
+  public Point get(int index){
+    return null;
+  }
+
+  // TODO: Create a stub for size()
+  public int size(){
+    return 0;
+  }
+
+  // TODO: Create a stub for lowestPoint()
+  public Point lowestPoint(){
+    return null;
+  }
+
+  // TODO: Create a stub for highestPoint()
+  public Point highestPoint(){
+    return null;
+  }
+
+  // TODO: Create a stub for totalDistance()
+  public double totalDistance(){
+    return 0;
+  }
+
+  // TODO: Create a stub for averageSpeed()
+  public double avergeSpeed(){
+    return 0;
+  }
+}
