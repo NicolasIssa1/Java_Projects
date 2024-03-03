63f7d863674eb4c9de614c9d63ef77faafb08d4b
NicolasIssa1 <112622885+NicolasIssa1@users.noreply.github.com>
2024-03-03 17:36:30 +0000

    done ex ex 7 now coursework

--- EXX/Circle.java
+++ EXX/Circle.java
@@ -1,12 +1,24 @@
 package EXX;
+import java.lang.Math;
+import java.lang.Override;
 
 public class Circle {
     private double radius;
 
-    public Circle(double r) {
-        radius = r;
+    public Circle() {
+        this(1);
     }
-    
+
+    public Circle(double r){
+        if(r <= 0){
+            throw new IllegalArgumentException("Invalid radius");
+        }
+        else
+        {
+            radius = r;
+        }
+    }
+ 
     public double getRadius(){
         return radius;
     }
@@ -18,4 +30,21 @@ public class Circle {
     public double perimeter(){
         return 2 * Math.PI * radius;
     }
-}
+
+    @Override
+    public String toString(){
+        return "Circle(radius=" + String.format("%.4f", radius) + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) {
+        return true;
+      }
+      if (obj instanceof Circle) {
+        Circle circ = (Circle) obj;
+        return Math.abs(radius - circ.radius) < 0.00005;
+      }
+      return false;
+    }
+  }
--- EXX/CircleDemo.java
+++ EXX/CircleDemo.java
@@ -2,9 +2,8 @@ package EXX;
 
 public class CircleDemo{
     public static void main(String[] args){
-        double radius = 4.5;
-
-        Circle myCircle = new Circle(radius);
+        Circle myCircle = new Circle();
+        Circle myCircle1 = new Circle();
 
         System.out.println("Radius = " + myCircle.getRadius());
         System.out.println("Perimeter = " + myCircle.perimeter());
